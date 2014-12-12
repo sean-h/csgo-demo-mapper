358a230bb584f9683fe45487d02f8726bdba1870
Sean Humeniuk <seanhumeniuk@gmail.com>
2014-12-12 15:38:59 -0500

    Add team filter and catch ValueError exceptions in get_positions

--- parse.py
+++ parse.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 import pylab as pl
 
-def get_positions(data):
+def get_positions(data, team_filter=None):
     x_positions = []
     y_positions = []
     for line in data:
-        x, y, z, team, weapon = line.split(' ')
-        x_positions.append(x)
-        y_positions.append(y)
+        try:
+            x, y, z, team, weapon = line.split(' ')
+            if team_filter is None or team == team_filter:
+                x_positions.append(float(x))
+                y_positions.append(float(y))
+        except ValueError as ex:
+            print ex, "\non line:\n", line, "\n"
     return x_positions, y_positions
 
 with open('shots.txt', 'r') as f:
@@ -16,7 +20,7 @@ with open('shots.txt', 'r') as f:
 lines = file_data.split('\n')
 del lines[-1]
 
-x, y = get_positions(lines)
+x, y = get_positions(lines, team_filter="CT")
 fig = pl.figure(figsize=(50, 50))
 axes = fig.add_axes([0, 0, 1, 1])
 axes.axis('off')
